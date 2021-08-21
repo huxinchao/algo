19513fcdf1df8405490a7768d9ffbea3eb754d96
huxinchao <1453056935@qq.com>
2021-08-21 18:30:19 +0800

    add run ignore

--- acm/2nd-length-path.cpp
+++ acm/2nd-length-path.cpp
@@ -6,28 +6,13 @@ using namespace std;
 
 #define INF (INT_MAX / 2)
 
-struct edge {
-	int from;
-	int to;
-	int cost;
-};
 
-edge es[100000];
-int d[5000][2];
 
-int N,R;
 int main() {
-	int k = 1;
-	
-	cin >> N >> R;
-	N--;
-
-	
-	
-
-
+	foo(2, 5);
 	return 0;
 }
+
 				
 			
 		
