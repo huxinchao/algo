a1fa58bb66cfdf74eda933ff7b7b44c61e30df65
huxinchao <1453056935@qq.com>
2021-08-21 14:50:07 +0800

    2nd-length-path.cpp

---
+++ acm/2nd-length-path.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include <limits.h>
+
+using namespace std;
+
+#define INF (INT_MAX / 2)
+
+struct edge {
+	int from;
+	int to;
+	int cost;
+};
+
+edge es[100000];
+int d[5000][2];
+
+int N,R;
+int main() {
+	int k = 1;
+	
+	cin >> N >> R;
+	N--;
+
+	
+	
+
+
+	return 0;
+}
+				
+			
+		
+	
+
+			
+		
+		
+		
+	
