198682864836de94f2522af700770d586c9e54e9
huxinchao <1453056935@qq.com>
2021-08-23 18:18:51 +0800

    exgcd

---
+++ acm/exgcd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int exgcd(int a, int b, int& x, int &y) {
+	int d = a;
+	if(b != 0) {
+		d = exgcd(b, a % b, y, x);
+		y -= (a / b) * x;
+	} else {
+		x = 1;
+		y = 0;
+	}
+	return d;
+}
+int main() {
+	int a = 4;
+	int b = 11;
+	int x,y;
+	exgcd(a,b,x,y);
+	cout << x << endl << y << endl;
+	return 0;
+ }
