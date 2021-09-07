2d19fee5e83defdd917af150ac25ab6022af0a38
huxinchao <1453056935@qq.com>
2021-09-07 20:37:13 +0800

    tsl

---
+++ acm/tsl.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <limits.h>
+#include <cstring>
+using namespace std;
+
+#define INF (INT_MAX / 2)
+const int maxn = 15;
+
+int N,L;
+int D[maxn][maxn];
+int Dp[1 << maxn][maxn];
+
+int rec(int S, int v) {
+	if(Dp[S][v] >= 0) {
+		return Dp[S][v];
+	}
+	if(S == (1 << N) - 1 && v == 0) {
+		return Dp[S][v] = 0;
+	}
+
+	int res = INF;
+	for(int u = 0; u < N; u++) {
+		if(!(S >> u & 1)) {
+			res = min(res, rec(S | 1 << u, u) + D[v][u]);
+		}
+	}
+	return Dp[S][v] = res;
+}
+
+int main() {
+	cin >> N >> L;
+	memset(D,0,sizeof(D));
+	
+	for(int i = 0; i < L; i++) {
+		int s,d,t;
+		cin >> s >> d >> t;
+		D[s][d] = t;
+	}
+	
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			if(i == j) D[i][j] = 0;
+			else if(D[i][j] == 0) D[i][j] = INF;
+		}
+	}
+
+	memset(Dp,-1,sizeof(Dp));
+	cout << rec(0,0) << endl;
+
+	
+}
+
+
+
+
---
+++ acm/tsl_b.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstring>
+#include <limits.h>
+using namespace std;
+
+const int maxn = 200;
+#define INF (INT_MAX / 2)
+
+int M,N,A,B,L;
+int Mp[maxn][maxn];
+double Dp[1 << 8][maxn];
+int T[maxn];
+
+int main() {
+	
+	cin >> N >> M >> A >> B;
+	for(int i = 0; i < N; i++) {
+		cin >> T[i];
+	}
+
+
+	for(int i = 0; i < M; i++) {
+		for(int j = 0; j < M; j++) {
+			if(i == j) Mp[i][j] = 0;
+			else Mp[i][j] = -1;
+		}
+	}
+
+	cin >> L;
+	for(int i = 0; i < L; i++) {
+		int s,d,t;
+		cin >> s >> d >> t;
+		Mp[s][d] = t;
+		Mp[d][s] = t;
+	}
+
+	for(int i = 0; i < 1 << N; i++) {
+		fill(Dp[i], Dp[i] + M, INF);
+	}
+
+	Dp[(1 << N) - 1][A - 1] = 0;
+	double res = INF;
+	for(int S = (1 << N) - 1; S >= 0; S--) {
+		res = min(res, Dp[S][B - 1]);
+		for(int v = 0; v < M; v++) {
+			for(int i = 0; i < N; i++) {
+				if(S >> i & 1) {
+					for(int u = 0; u < M; u++) {
+						if(Mp[v][u] >= 0) {
+							Dp[S & ~(1 << i)][u] = min(Dp[S & ~(1 << i)][u], Dp[S][v] +
+													   (double)Mp[v][u] / T[i]);
+						}
+					}
+				}
+			}
+		}
+	}
+	if(res == INF) cout << "Impossible" << endl;
+	else cout << res << endl;
+
+	return 0;
+}
