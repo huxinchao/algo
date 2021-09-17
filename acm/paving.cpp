1e8241551fcf76b90e2b12647234faf0ed84f14f
huxinchao <1453056935@qq.com>
2021-09-18 05:01:21 +0800

    paving.cpp

--- acm/paving.cpp
+++ acm/paving.cpp
@@ -1,51 +1,41 @@
-#include <iostream>
-#include <bitset>
+#include<bits/stdc++.h>
 using namespace std;
-
-int N,M;
-long long Mod;
-int Mp[15][15];
-int Dp[2][1 << 15];
-
-int main() {
-	cin >> N >> M;
-	for(int i = 0; i < N; i++) {
-		for(int j = 0; j < M; j++) {
-			char c;
-			cin >> c;
-			if(c == '.') Mp[i][j] = 0;
-			if(c == 'x') Mp[i][j] = 1;
-		}
-	}
-
-	int *crt = Dp[0];
-	int *nxt = Dp[1];
-
-	crt[0] = 1;
-	for(int i = N - 1; i >= 0; i--) {
-		for(int j = M - 1; j >= 0; j--) {
-			for(int used = 0; used < 1 << M; used++) {
-				if((used >> j & 1) || Mp[i][j]) {
-					nxt[used] = crt[used & ~(1 << j)];
-				} else {
-					int res = 0;
-					if(j + 1 < M && !(used >> (j + 1) & 1) && !Mp[i][j+1]) {
-						res += crt[used | 1 << (j + 1)];
-					}
-					if(i + 1 < N && !Mp[i+1][j]) {
-						res += crt[used | 1 << j];
-					}
-					nxt[used] = res;
-				}
-				bitset<4> bs = used;
-				cout << bs << " " << nxt[used] << endl;
-				
-				
+bool M[17][17];
+int dp[2][1<<15]; 
+int main(){
+	int n,m;
+	cin >> n >> m;
+		memset(dp,0,sizeof(dp));
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){ //从0开始后面会好操作一点 
+				char input; cin>>input;
+				if(input=='.') M[i][j]=0; //0:白 1：黑，将0全变为1 
+				else M[i][j]=1;
 			}
-			swap(crt, nxt);
 		}
-	}
-	cout << crt[0] << endl;
-	return 0;
-	
+		int *now=dp[0],*nex=dp[1];
+		now[0]=1;
+		for(int i=n-1;i>=0;i--){ //从n-1 开始会方便二进制表示状态 
+			for(int j=m-1;j>=0;j--){
+				for(int used=0;used< (1<<m) ;used++){ //遍历状态,这里反过来表示  
+					if(used>>j & 1 || M[i][j]){ //假如这个位置被用了或者是1 不用填 
+						nex[used]=now[used & ~(1<<j)];  
+					} 
+					else{
+						int res=0;
+						if(j+1<m && !(used>>(j+1)&1) && !M[i][j+1]){ //横着放 
+							res+=now[used|1<<(j+1)];
+						}
+						if(i+1<n && !M[i+1][j] ){ //竖着放 
+							res+=now[used|1<<j];
+						}
+						nex[used]=res;
+					} 
+					
+				}
+				swap(now,nex);
+			}
+		}
+		cout<<now[0]<<endl;
+	return 0; 
 }
