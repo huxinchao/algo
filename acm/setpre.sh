e7499d42dd742c561090559e7755cc604d76ffb2
huxinchao <1453056935@qq.com>
2021-09-24 17:24:42 +0800

    .gitignore

--- acm/setpre.sh
+++ acm/setpre.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-files=$(ls *.cpp)
+files=$(ls *.cpp 2>/dev/null)
+
+if [ $? -eq 0 ]; then
+    echo "[+] Getting cpp files"
+else
+    echo "[-] Can't find cpp files"
+fi
+
+
 for file in ${files}
 do
     echo "Process: ${file}"
     sed -i "1i\//  Filename: ${file}" ${file}
-    
-
 done
