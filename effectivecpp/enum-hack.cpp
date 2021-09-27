def495d9af4eb55c5ac7efa64430202cf56759e9
huxinchao <1453056935@qq.com>
2021-09-28 03:54:11 +0800

    enum-hack

---
+++ effectivecpp/enum-hack.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class A {
+    enum { Num_b = 5 };
+    static const int Num = 5;
+    const int Num_c = 5;
+    int arr[Num_b];
+
+};
+const int A::Num;
+
+int main() {
+    
+
+    
+
+
+    return 0;
+}
