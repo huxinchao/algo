3c53f1322c659019d75689530544f212e0ce42d6
huxinchao <1453056935@qq.com>
2022-02-17 16:53:57 +0800

    new files

--- effectivecpp/function.cpp
+++ effectivecpp/function.cpp
@@ -2,18 +2,19 @@
 #include <functional>
 
 class Base {
+public:
+    int v;
+
 };
 
 class Derived : public Base{
+
 };
 
-int fun(const Base& d) {
-    return 0;
-}
-
 int main() {
-    using namespace std;
-    function<int(const Base&)> f(&fun);
+
+    
+    
     
 }
 
